0a58869c6e322f56ac5db64c6355e0a2bfbc6473
efe3535 <a.efe.akyazi@gmail.com>
2021-10-31 20:03:43 +0300

    Remove welcome.json

--- enginarabout.js
+++ enginarabout.js
@@ -1,12 +1,28 @@
 #!/usr/bin/env node
 const img = require('terminal-image');
 const got = require('got')
-const welcome = JSON.parse(require("fs").readFileSync("welcome.json", { encoding: "utf8" }));
+const texts = JSON.parse(require("fs").readFileSync("welcome.json", { encoding: "utf8" }));
+const texts = [
+    "\u001b[33m\u001b[00m A wild \u001b[36mEfe\u001b[00m appeared!",
+    "\u001b[33m\u001b[00m Welcome, \u001b[36mEfe\u001b[00m. We hope you brought pizza.",
+    "\u001b[33m\u001b[00m Good to see you, \u001b[36mEfe\u001b[00m.",
+    "\u001b[33m\u001b[00m \u001b[36mEfe\u001b[00m just showed up!",
+    "\u001b[33m\u001b[00m \u001b[36mEfe\u001b[00m joined the party.",
+    "\u001b[33m\u001b[00m Glad you're here, \u001b[36mEfe\u001b[00m.",
+    "\u001b[33m\u001b[00m Yay you made it, \u001b[36mEfe\u001b[00m.",
+    "\u001b[33m\u001b[00m \u001b[36mEfe\u001b[00m just slid into the your console.",
+    "\u001b[33m\u001b[00m \u001b[36mEfe\u001b[00m just landed.",
+    "\u001b[33m\u001b[00m \u001b[36mEfe\u001b[00m is here.",
+    "\u001b[33m\u001b[00m Wellcome \u001b[36mEfe\u001b[00m.",
+    "\u001b[33m\u001b[00m Everyone wellcome \u001b[36mEfe\u001b[00m",
+    "\u001b[33m\u001b[00m \u001b[36mEfe\u001b[00m hopped into the your console.",
+    "\u001b[33m\u001b[00m Wellcome \u001b[36mEfe\u001b[00m. Say hi!"
+]
 
 got("https://avatars.githubusercontent.com/u/46342237?s=195", { responseType: 'buffer' })
 	.then(function (image) { return img.buffer(image.body, { width: '33%' }) })
 	.then(function (image) {
-		console.log(welcome[Math.floor(Math.random() * welcome.length)]);
+		console.log(texts[Math.floor(Math.random() * texts.length)]);
 		console.log(image);
 		console.log("Hello, I'm  \u001b[32mEfe\u001b[00m.\nI love exploring something new, that is why I'm learning \u001b[31m programming\u001b[00m.\nI'm interested in \u001b[34mLinux\u001b[00m and \u001b[35mC\u001b[00m.")
 	});
